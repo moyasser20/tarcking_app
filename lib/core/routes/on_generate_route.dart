6a02db6f380ab5c0352f03770fbc741add60af0a
moyasser20 <mohamedyasser192023@gmail.com>
2025-09-23 19:23:46 +0300

    Finish onboarding Screen , add localization files

--- lib/core/routes/on_generate_route.dart
+++ lib/core/routes/on_generate_route.dart
@@ -1,10 +1,15 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tarcking_app/core/routes/route_names.dart';
+
+import '../../features/onboarding/presentation/view/onboarding_screen.dart';
 
 class Routes {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case AppRoutes.initial:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
 
       default:
         return MaterialPageRoute(
---
+++ lib/features/onboarding/presentation/view/onboarding_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:tarcking_app/core/extensions/extensions.dart';
+import 'package:tarcking_app/core/theme/app_colors.dart';
+import 'package:tarcking_app/core/widgets/custom_Elevated_Button.dart';
+
+import '../../../../core/l10n/translation/app_localizations.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: Column(
+        children: [
+          const SizedBox(height: 60),
+          Image.asset("assets/images/delivery_image.png"),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              local.welcomeTo,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+                color: AppColors.black,
+                fontFamily: "Inter",
+              ),
+            ),
+          ).setHorizontalPadding(context, 0.045),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              local.floweryRiderApp,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+                color: AppColors.black,
+                fontFamily: "Inter",
+              ),
+            ),
+          ).setHorizontalPadding(context, 0.045),
+          const SizedBox(height: 20),
+          CustomElevatedButton(text: local.login, onPressed: () {}),
+          const SizedBox(height: 20),
+          CustomElevatedButton(
+            text: local.applyNow,
+            borderColor: AppColors.grey.withOpacity(0.5),
+            onPressed: () {},
+            color: AppColors.white,
+            textColor: AppColors.grey.withOpacity(0.8),
+          ),
+          const SizedBox(height: 200),
+          Text(
+            local.versionText,
+            style: TextStyle(color: AppColors.grey),
+          )
+        ],
+      ),
+    );
+  }
+}
